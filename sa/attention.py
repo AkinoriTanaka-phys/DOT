50a004e3b59d723206d8c0f120da3a8d5e36a2f1
akinori tanaka <akinori.physics@gmail.com>
2019-10-27 00:30:14 +0900

    chainer SA implementation based on https://github.com/heykeetae/Self-Attention-GAN/blob/master/sagan_models.py

--- sa/attention.py
+++ sa/attention.py
@@ -8,10 +8,9 @@ from snd.sn_convolution_2d import SNConvolution2D
 
 
 class Self_Attn(chainer.Chain):
-    def __init__(self, in_dim):#, activation):
+    def __init__(self, in_dim):
         super(Self_Attn, self).__init__()
         self.chanel_in = in_dim
-        #self.activation = activation
         with self.init_scope():
             self.query_conv = SNConvolution2D(in_channels = in_dim, out_channels = in_dim//8, ksize=1, \
                                           stride=1, pad=0, nobias=False)
@@ -21,7 +20,6 @@ class Self_Attn(chainer.Chain):
                                           stride=1, pad=0, nobias=False)
             self.gamma = chainer.Parameter(initializer=chainer.initializers.Zero(), shape=(1,))
 
-        #self.softmax  = F.softmax(axis=-1) #
     def __call__(self,x):
         """
             inputs :
@@ -31,20 +29,15 @@ class Self_Attn(chainer.Chain):
                 attention: B X N X N (N is Width*Height)
         """
         m_batchsize, C, width, height = x.shape
-        #print(self.query_conv(x).shape)
         proj_query  = F.transpose(F.reshape(self.query_conv(x), (m_batchsize, -1, width*height)),\
                                   (0,2,1))
-        #print(proj_query.shape) # B x C x N
         proj_key =  F.reshape(self.key_conv(x), (m_batchsize, -1, width*height))
-        #print(proj_key.shape) # B x C x N
         energy =  F.matmul(proj_query, proj_key)
-        #torch.bmm(proj_query, proj_key) # transpose check
         attention = F.softmax(energy, axis=2) # B x N x N 
         proj_value = F.reshape(self.value_conv(x), (m_batchsize, -1, width*height)) # B X C X N
 
         out = F.matmul(proj_value, F.transpose(attention, (0,2,1)))
         out = F.reshape(out, (m_batchsize,C,width,height))
-        #print(self.gamma)
         
         out = F.broadcast_to(self.gamma, out.shape)*out + x
         return out, attention
