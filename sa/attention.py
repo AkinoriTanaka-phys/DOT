8d463bf3607954a42daa673c00f57c35e980fc94
akinori tanaka <akinori.physics@gmail.com>
2019-11-09 20:37:51 +0900

    updated

--- sa/attention.py
+++ sa/attention.py
@@ -13,28 +13,20 @@ class Self_Attn(chainer.Chain):
         self.chanel_in = in_dim
         with self.init_scope():
             self.query_conv = SNConvolution2D(in_channels = in_dim, out_channels = in_dim//8, ksize=1, \
-                                          stride=1, pad=0, nobias=False)
+                                              stride=1, pad=0, nobias=False)
             self.key_conv = SNConvolution2D(in_channels = in_dim, out_channels = in_dim//8, ksize=1, \
-                                          stride=1, pad=0, nobias=False)
+                                            stride=1, pad=0, nobias=False)
             self.value_conv = SNConvolution2D(in_channels = in_dim, out_channels = in_dim, ksize=1, \
-                                          stride=1, pad=0, nobias=False)
+                                              stride=1, pad=0, nobias=False)
             self.gamma = chainer.Parameter(initializer=chainer.initializers.Zero(), shape=(1,))
 
     def __call__(self,x):
-        """
-            inputs :
-                x : input feature maps( B X C X W X H)
-            returns :
-                out : self attention value + input feature 
-                attention: B X N X N (N is Width*Height)
-        """
         m_batchsize, C, width, height = x.shape
-        proj_query  = F.transpose(F.reshape(self.query_conv(x), (m_batchsize, -1, width*height)),\
-                                  (0,2,1))
+        proj_query  = F.transpose(F.reshape(self.query_conv(x), (m_batchsize, -1, width*height)), (0,2,1))
         proj_key =  F.reshape(self.key_conv(x), (m_batchsize, -1, width*height))
         energy =  F.matmul(proj_query, proj_key)
-        attention = F.softmax(energy, axis=2) # B x N x N 
-        proj_value = F.reshape(self.value_conv(x), (m_batchsize, -1, width*height)) # B X C X N
+        attention = F.softmax(energy, axis=2) 
+        proj_value = F.reshape(self.value_conv(x), (m_batchsize, -1, width*height))
 
         out = F.matmul(proj_value, F.transpose(attention, (0,2,1)))
         out = F.reshape(out, (m_batchsize,C,width,height))
